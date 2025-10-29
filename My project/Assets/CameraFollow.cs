47ee8f5839bf8d8659587497db7549b524ef8d1a
ibrahimbahaa35 <ibrahim247396@bue.edu.eg>
2025-10-29 04:41:18 -0700

    Material

---
+++ My project/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float Cameraspeed;
+    public float minX, maxX;
+    public float minY, MaxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Target != null) {
+            Vector2 newCamPosition = Vector2.Lerp(transform.position, Target.position, Time.deltaTime* Cameraspeed);
+            float ClampX = Mathf.Clamp(newCamPosition.x, minX, maxX);
+            float ClampY = Mathf.Clamp(newCamPosition.y, minY, MaxY);
+            transform.position = new Vector3(ClampX, ClampY, -10f);
+        }
+    }
+}
