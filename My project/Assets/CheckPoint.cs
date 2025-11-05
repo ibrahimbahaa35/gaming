26b6e3b8d77b1cd5cd9d09bebd5e574ae85b47ea
ibrahimbahaa35 <ibrahim247396@bue.edu.eg>
2025-11-05 04:43:49 -0800

    New

---
+++ My project/Assets/CheckPoint.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.tag == "Player") {
+            FindObjectOfType<LevelManager>().CurrentCheckPoint = this.gameObject;
+        }
+    }
+}
